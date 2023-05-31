1f4f4faa1c8fe8c3bf77b21a8f5803c123920657
Speed Racer <drspeedracer@Speeds-iMac.local>
2023-05-31 10:31:59 -0400

    updating dates and times

--- config/resy-config.go
+++ config/resy-config.go
@@ -38,7 +38,7 @@ var ResTimeTypes = []ReservationTimeType{
     NewReservationTimeType("18:30:00", &tableType),
 }
 var ReservationDetailss = ReservationDetails{
-    Date:         "2023-06-04",
+    Date:         "2023-06-07",
     PartySize:    2,
     VenueId:      466,
     ResTimeTypes: ResTimeTypes,
